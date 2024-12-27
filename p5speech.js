93000171798abf9311165a043e25e3260a208efe
s79082 <s79082@htw-dresden.de>
2024-12-28 00:36:44 +0100

    adds p5 speech recognition, ws send

---
+++ p5speech.js
@@ -0,0 +1,54 @@
+// A2Z F18
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F18
+
+// Speech Object
+let speech;
+
+
+const socket = new WebSocket('ws://localhost:8080');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server soos!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+sendMsg = (msg) => {
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(msg)
+  }
+}
+
+function setup() {
+  noCanvas();
+  // Create a Speech Recognition object with callback
+  speechRec = new p5.SpeechRec('en-US', gotSpeech);
+  // "Continuous recognition" (as opposed to one time only)
+  let continuous = true;
+  // If you want to try partial recognition (faster, less accurate)
+  let interimResults = true;
+  // This must come after setting the properties
+  speechRec.start(continuous, interimResults);
+
+  // DOM element to display results
+  let output = select('#speech');
+
+  // Speech recognized event
+  function gotSpeech() {
+    // Something is there
+    // Get it as a string, you can also get JSON with more info
+    console.log(speechRec);
+    if (speechRec.resultValue) {
+      let said = speechRec.resultString;
+      // Show user
+      output.html(said);
+      sendMsg(said);
+    }
+  }
+}
